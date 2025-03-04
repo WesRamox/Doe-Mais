e7e764886c8243e4a4a82c7c02be43c9c8ff55a4
WesRamox <wesleyramox258@gmail.com>
2025-03-04 15:10:12 -0300

    Instalando e configurando Jest

---
+++ api/__tests__/controllers/campaignsController.spec.ts
@@ -0,0 +1,9 @@
+// DESCRIBE -> Bloco tests - Tests suites
+// IT or Test -> Declara unico Teste unitário - Tests cases
+// EXPECT -> asserções do resultado do teste - Validação de resultados
+
+describe('Campaigns Controller', () => {
+  test("", () => {
+    
+  })
+})
