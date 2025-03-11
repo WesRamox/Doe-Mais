0bd341dec3703c64f6e75d156a432db6490e1f03
WesRamox <wesleyramox258@gmail.com>
2025-03-11 20:05:09 -0300

    feat: add /me route to fetch logged user details

--- api/src/routes/authRoutes.ts
+++ api/src/routes/authRoutes.ts
@@ -1,9 +1,13 @@
 import express from "express";
 import { registerUser, loginUser } from "../controllers/authController";
+import { authenticate } from "../middlewares/authMiddleware";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.get("/me", authenticate, (req, res) => {
+  res.json(req.user)
+});
 
 export default router;
